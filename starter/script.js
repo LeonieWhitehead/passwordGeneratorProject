71f94651e809bac5141d8e24f7765e9b4b3071c5
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-01-08 14:09:29 +0000

    cleaned up code. Put variables inside getpassword function

--- starter/script.js
+++ starter/script.js
@@ -88,123 +88,89 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// 1. create prompt messages
-// 2. create variables for each prompt option
-
-let passwordLength;
-let passwordLower;
-let passwordUpper;
-let passwordNumeric;
-let passwordSpecial;
-
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //prompt user for length of password and turn password length into number?
+  var passwordLength = parseInt(prompt('How many characters would you like your password to contain? (8 - 128)'));
 
-  passwordLength = prompt('How many characters would you like your password to contain? (8 - 128)');
-
+  // if cancelled alert user
   if (passwordLength === null) {
-    alert('Password cancelled. Please try again!');
+    alert('Password cancelled!');
   }
-  //turn password length into number?
-  passwordLength = parseInt(passwordLength);
 
-  // multiple conditions in one. Greater than and equal to, to put in one if statement?
-  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength> 128) {
+  //validate length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert('Password must be between 8 and 128 characters. Please try again!');
-    
-    return passwordLength; 
-    // How to make prompt pop up until correct amount of characters input? else statement? Can you return a prompt? for loop?
-    // while(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    //    passwordLength = prompt('How many characters would you like your password to contain? (8 - 128)');
-    //    passwordLength = parseInt(passwordLength);
-    // }
-    // while (isNaN(passwordLength) || passwordLength < 8 || passwordLength> 128) {
-    //   alert('Password must be between 8 and 128 characters. Please try again!');
+    passwordLength = parseInt(prompt('How many characters would you like your password to contain? (8 - 128)'));
+
   }
 
-  //need if statement for confirms or cancels?
-    passwordLower = confirm('Click OK to confirm or cancel to decline lower case characters');
-  //   if(passwordLower === true){
-  //   console.log('yes');
-  // } else if(passwordLower === false);{
-  //   console.log('no');
-  // }
+  //prompt for character types
+  var includeLower = confirm('Click OK to confirm or cancel to decline lower case characters');
+  console.log('iL');
 
-  // if(passwordLower == true){
-  passwordUpper = confirm('Click OK to confirm or cancel to decline upper case characters');
-  // if(passwordUpper === true){
-  //   console.log('yes');
-  // } else(passwordUpper === false);{
-  //   console.log('no');
-  // }
+  var includeUpper = confirm('Click OK to confirm or cancel to decline upper case characters');
+  console.log('iU');
 
-  passwordNumeric = confirm('Click OK to confirm or cancel to decline numeric characters');
-  // if(passwordNumeric === true){
-  //   console.log('yes');
-  // } else(passwordNumeric === false);{
-  //   console.log('no');
-  // }
+  var includeNumeric = confirm('Click OK to confirm or cancel to decline numeric characters');
+  console.log('iN');
 
-  passwordSpecial = window.confirm('Click OK to confirm or cancel to decline special characters');
-  // if(passwordSpecial === true){
-  //   console.log('yes');
-  // } else(passwordSpecial === false);{
-  //   console.log('no');
-  // }
+  var includeSpecial = window.confirm('Click OK to confirm or cancel to decline special characters');
+  console.log('iS');
 
-  //if statement in one line?
+  //validate that at least one character type is selected
+  while (!(includeLower || includeUpper || includeNumeric || includeSpecial)) {
+    alert("At least one character type should be selected.");
+    includeLowercase = confirm("Include lowercase characters?");
+    includeUppercase = confirm("Include uppercase characters?");
+    includeNumeric = confirm("Include numeric characters?");
+    includeSpecial = confirm("Include special characters?");
+  }
+  // Return object with selected options
 
-  //object for password options? All choices organised in one section, easier to call?
-
-  let passwordOptions = {
+  return {
     passwordLength: length,
-    passwordLower: lower,
-    passwordUpper: upper,
-    passwordNumeric: numeric,
-    passwordSpecial: special
+    includeLower: includeLower,
+    includeUpper: includeUpper,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial
   };
-
-
-  return passwordOptions;
   
 }
- let passwordOptions = getPasswordOptions();
-console.log(passwordOptions);
+// var passwordOptions = getPasswordOptions();
+// console.log(getPasswordOptionspasswordOptions);
+
+  // Function for getting a random element from an array. Use .length to go through whole array. Create variable for random index
+  function getRandom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  // Function to generate password with user input
+  function generatePassword() {
+    // getPasswordOptions ();
+
+    // if (getPasswordOptions.lower) ;
+    // if (getPasswordOptions.upper) ;
+    // if (getPasswordOptions.numeric);
+    // if (getPasswordOptions.special);
+
+    // for (let i = 0; i < getPasswordOptions.length; i++) {
+ }
 
 
-// Function for getting a random element from an array. Use .length to go through whole array. Create variable for random index
-function getRandom(arr) {
-  let randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
-}
+    // return "";
 
-// Function to generate password with user input
-function generatePassword() {
-getPasswordOptions ();
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
 
-if (getPasswordOptions.lower) ;
-if (getPasswordOptions.upper) ;
-if (getPasswordOptions.numeric);
-if (getPasswordOptions.special);
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
 
-for (let i = 0; i < getPasswordOptions.length; i++) {
-  
-}
+    passwordText.value = password;
+  }
 
-  
-return "";
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
